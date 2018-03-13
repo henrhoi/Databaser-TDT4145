35e8810625657a217036caef406703aabfee064c
Henrik Høiness <henrhoi@stud.ntnu.no>
2018-03-13 15:56:18 +0100

    Laget DBConnection samt et par klasser som trengs

---
+++ Databaser-TDT4145/src/Exercise.java
@@ -0,0 +1,8 @@
+
+public class Exercise {
+	
+	public Exercise() {
+		
+	}
+
+}
---
+++ Databaser-TDT4145/src/Workout.java
@@ -0,0 +1,24 @@
+import java.sql.Date;
+import java.sql.Time;
+import java.util.List;
+
+public class Workout {
+	
+	private Date dato;
+	private Time tidspunkt;
+	private Time varighet;
+	private int personligForm;
+	private int prestasjon;
+	private String notat;
+	
+	private String exerciseGroup;
+	
+	private List<Exercise> exerciseList;
+	
+	public Workout() {
+		
+	}
+	
+	
+
+}
